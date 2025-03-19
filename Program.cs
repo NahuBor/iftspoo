a0b4145ed2218e6b8eb9836809fb92078952724e
NahuBor <nahuelemilianoborja33@gmail.com>
2025-03-18 21:27:36 -0300

    Create Program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+namespace PracticePoo
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Try programiz.pro");
+    }
+  }
+}
+
